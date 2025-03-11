c269fe7c1f902ee2b82adf3dfec747477b769538
jinie <jiniejinie001125@gmail.com>
2025-03-11 02:11:01 -0600

    Checking functions with MySQLAuthDAO.java.

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -23,14 +23,14 @@ public class MySQLAuthDAO implements AuthDAO {
     }
 
     @Override
-    public AuthData getAuth(String token) throws DataAccessException {
+    public AuthData getAuth(String authToken) throws DataAccessException {
         String sql = "SELECT * FROM auth_tokens WHERE authToken = ?";
         try (Connection conn = DatabaseManager.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, token);
+            stmt.setString(1, authToken);
             ResultSet rs = stmt.executeQuery();
             if (rs.next()) {
-                return new AuthData(rs.getString("token"), rs.getString("username"));
+                return new AuthData(rs.getString("authToken"), rs.getString("username"));
             }
             return null;
         } catch (SQLException e) {
@@ -39,11 +39,11 @@ public class MySQLAuthDAO implements AuthDAO {
     }
 
     @Override
-    public void deleteAuth(String token) throws DataAccessException {
+    public void deleteAuth(String authToken) throws DataAccessException {
         String sql = "DELETE FROM auth_tokens WHERE authToken = ?";
         try (Connection conn = DatabaseManager.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, token);
+            stmt.setString(1, authToken);
             stmt.executeUpdate();
         } catch (SQLException e) {
             throw new DataAccessException("Error deleting auth token: " + e.getMessage());
