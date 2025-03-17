0204a3a97f52402bb5bfb965ee9531f1e692e4a0
jinie <jiniejinie001125@gmail.com>
2025-03-16 20:54:54 -0600

    1, Getting started.

--- shared/src/main/java/model/GameData.java
+++ shared/src/main/java/model/GameData.java
@@ -4,15 +4,19 @@ import chess.ChessGame;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import com.google.gson.reflect.TypeToken;
+import java.lang.reflect.Type;
+
 public record GameData(int gameID, String whiteUsername, String blackUsername, String gameName, ChessGame game) {
 
-    private static final Gson GSON = new GsonBuilder().excludeFieldsWithoutExposeAnnotation().setPrettyPrinting().create();  // 직렬화 문제 해결을 위한 설정 추가
+    private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();  // 직렬화 문제 해결을 위한 설정 추가
 
     public String gameState() {
         return GSON.toJson(game); // ChessGame을 JSON으로 변환
     }
 
     public static ChessGame fromGameState(String gameStateJson) {
+        Type type = new TypeToken<ChessGame>() {}.getType();
         return GSON.fromJson(gameStateJson, ChessGame.class); // JSON을 ChessGame 객체로 변환
     }
 }
